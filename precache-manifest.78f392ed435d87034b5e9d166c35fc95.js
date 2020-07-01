146259569677bf4a87e032e41b79d0bcb78eaf4d
Maroua Ammar <maroi.ammar22@gmail.com>
2020-07-01 09:32:26 +0200

    Updates

---
+++ precache-manifest.78f392ed435d87034b5e9d166c35fc95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65132949d413bc4c7a1685f744609fd8",
+    "url": "/modele/index.html"
+  },
+  {
+    "revision": "40cf94df319792feeba2",
+    "url": "/modele/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "85ec73d17f3fa5fd4986",
+    "url": "/modele/static/js/2.0988d061.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/modele/static/js/2.0988d061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40cf94df319792feeba2",
+    "url": "/modele/static/js/main.1fb17b0f.chunk.js"
+  },
+  {
+    "revision": "d1377df406dc5e9c4b74",
+    "url": "/modele/static/js/runtime-main.aaf3590e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/modele/static/media/logo.5d5d9eef.svg"
+  }
+]);
