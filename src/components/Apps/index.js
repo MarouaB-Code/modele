bf789c65a6756df74eaa49d6a813913a96bba501
Maroua Ammar <maroi.ammar22@gmail.com>
2020-07-01 10:23:48 +0200

    app modified

--- src/components/Apps/index.js
+++ src/components/Apps/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../../App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
         <p>
           Edit and save to reload.
         </p>
